7278878e4fe9ab36ced3eb9f0760f7ef932f61f1
AHQMiness <84524025+ahqsoftwares@users.noreply.github.com>
2022-07-29 19:24:37 +0530

    check mapped items

--- src/Hooks/types/push.js
+++ src/Hooks/types/push.js
@@ -1,3 +1,37 @@
-function handle(payload, client) {
-         
+function handle({commits}, client) {
+         const {YELLOW} = require("./colors");
+         let mapped = commits.map((commit) => [commit.added, commit.removed, commit.modified, commit.author.name]),
+         added = mapped.map(([a, b, c, d]) => a).join(", "),
+         removed = mapped.map(([a, b, c, d]) => b).join(", "),
+         modified = mapped.map(([a, b, c, d]) => c).join(", "),
+         contributors;
+         mapped.map(([a, b, c , d]) => `[${d}](https://github.com/${d})`).forEach((e) => contributors.push(e));
+
+
+
+         client.executeWebhook(payload.id, payload.token, {
+                  embeds: [{
+                           title: `New Push on master!`,
+                           color: YELLOW,
+                           description: "Commit Details",
+                           fields: [{
+                                    name: "Added",
+                                    value: added,
+                                    inline: true
+                           }, {
+                                    name: "Removed",
+                                    value: removed,
+                                    inline: true
+                           }, {
+                                    name: "Modified",
+                                    value: modified,
+                                    inline: true
+                           }, {
+                                    name: "Contributors",
+                                    value: contributors.join(", "),
+                                    inline: false
+                           }]
+                  }],
+                  username: "AHQ Store Github Push"
+         });
 }
